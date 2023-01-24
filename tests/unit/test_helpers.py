5b0c1e9009246805174e86a53d043ada9c0a3bc5
JoshuaWeissTBS <joshua.weiss@hotmail.com>
2023-01-24 12:03:46 -0800

    test case 'test_less_than_day_with_one_minute' passing

--- tests/unit/test_helpers.py
+++ tests/unit/test_helpers.py
@@ -3,3 +3,7 @@ import app.helpers
 def test_less_than_day_with_less_than_minute():
     text = app.helpers.less_than_day(30)
     assert text == "30 seconds ago" 
+
+def test_less_than_day_with_one_minute():
+    text = app.helpers.less_than_day(90)
+    assert text == "a minute ago" 
